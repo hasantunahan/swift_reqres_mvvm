380605ad938333284bddbb954e3f767d3febb3f6
hasantunahan <hasan.tunahan.ak@gazi.edu.tr>
2021-07-15 19:51:39 +0300

    Initial Commit

---
+++ swift_example/swift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_exampleApp.swift
+//  swift_example
+//
+//  Created by HasanTunahan on 15.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
